49d98cf31ba3ec1d128672f2294df715478c871c
aryan14617 <85086993+aryan14617@users.noreply.github.com>
2025-09-05 20:21:20 +0530

    Update Likes.jsx

--- propose/src/Likes.jsx
+++ propose/src/Likes.jsx
@@ -2,7 +2,8 @@ export default function Likes() {
     return (
         <div className="centered-page">
             <h3>Thannksss!!!!</h3>
-            <h4>I appreciate you that you respected both of our feelings for each other <br /> Now keep the phone away and let's talk in real life ;) </h4>
+            <h4>I appreciate you that you respected both of our feelings for each other <br /> Now let's talk in real life ;) </h4>
         </div>
     );
 }
+
