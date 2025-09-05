65a5a59742481a133ceda671d625ab76b0bb6eec
aryan14617 <85086993+aryan14617@users.noreply.github.com>
2025-09-05 20:09:07 +0530

    Update Promised.jsx

--- propose/src/Promised.jsx
+++ propose/src/Promised.jsx
@@ -8,7 +8,7 @@ export default function Promised() {
             <h2>Calmly read following lines.</h2>
             <br />
             <p style={{fontWeight:"bold"}}>
-                Okay, so you have promised that you won't be angry. I took this promise so that I don't hurt you unknowningly. I want to tell you that I LIKE YOU since I saw you first time 7-8 years ago. I didn't have the courage to ask this to you directly so I chose this way. I want to ask that &mdash; <br /> <span style={{fontStyle: "italic"}}>Do you also have feelings any for me too?</span></p>
+                Okay, so you have promised that you won't be angry. I took this promise so that I don't hurt you unknowningly. I want to tell you that I LIKE YOU since I saw you first time 7-8 years ago. I didn't have the courage to ask this to you directly so I chose this way. I want to ask that &mdash; <br /> <span style={{fontStyle: "italic"}}>Do you also have any feelings for me too?</span></p>
             <br />
             <div style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
                 <button style={{backgroundColor: "white", border: "2px solid #007bff", color: "#007bff"}} onClick={() => navigate("/likes")}>Yes!!!</button>
@@ -17,3 +17,4 @@ export default function Promised() {
         </div>
     );
 }
+
